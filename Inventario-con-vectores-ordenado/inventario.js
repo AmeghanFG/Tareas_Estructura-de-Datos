c6c81699e7873d9c3f876659abcca2608f1b645c
Meghan <afigueroa57@ucol.mx>
2025-10-05 23:03:43 -0600

    Corrección de comentarios

--- Inventario-con-vectores-ordenado/inventario.js
+++ Inventario-con-vectores-ordenado/inventario.js
@@ -1,15 +1,3 @@
-// Datos producto:  código, nombre, cantidad y costo
-
-/*
-agregar -
-buscar por código -
-eliminar por código -
-recuperar el listado como texto -
-extraer el primer elemento (devolverlo y eliminarlo)  -
-extraer el último elemento
-recuperar el listado en orden inverso -
-*/
-
 class Inventario {
   constructor() {
     this.productos = [];
